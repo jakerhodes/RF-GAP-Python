df8c6134484d1bfd54e1f5512626406b39128947
Jake Rhodes <jakerhodes@isu.edu>
2023-04-06 14:42:29 -0600

    added dataset.py

---
+++ dataset.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+
+
+# TODO: Rewrite this as a pipeline, either sklearn or pandas
+def dataprep(data, label_col_idx = 0, scale = 'normalize'):
+
+    data = data.copy()
+    categorical_cols = []
+    for col in data.columns:
+        if data[col].dtype == 'object' or data[col].dtype == 'int64':
+            categorical_cols.append(col)
+            data[col] = pd.Categorical(data[col]).codes
+
+
+    if label_col_idx is not None:
+        label = data.columns[label_col_idx]
+        y     = data.pop(label)
+        x     = data
+
+
+    if scale == 'standardize':
+        for col in x.columns:
+            # if col not in categorical_cols:
+            if data[col].std() !=0:
+                data[col] = (data[col] - data[col].mean()) / data[col].std()
+
+    elif scale == 'normalize':
+        for col in x.columns:
+            # if col not in categorical_cols:
+            if data[col].max() != data[col].min():
+                data[col] = (data[col] - data[col].min()) / (data[col].max() - data[col].min())
+
+    if label_col_idx is None:
+        return np.array(x)
+    else:
+        return np.array(x), y
+
