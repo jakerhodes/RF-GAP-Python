84742ad53eea8393e50803b87de6cba5571bc90b
Jake Rhodes <jakerhodes8@gmail.com>
2024-04-18 11:32:34 -0600

    add qrf interval results

--- qrf_loop.py
+++ qrf_loop.py
@@ -16,7 +16,7 @@ import multiprocessing
 from joblib import Parallel, delayed
 import os
 
-from skgarden import RandomForestQuantileRegressor
+from quantile_forest import RandomForestQuantileRegressor
 
 sns.set_theme()
 
@@ -44,14 +44,14 @@ def interval_loop(data_name = None, level = 0.95, random_state = 42, test_size =
         str(level) + '_' + str(random_state) + '_' + 
         str(test_size) + '_QRF.json')
 
-    if os.path.isfile('intervals/' + filename):
+    if os.path.isfile('qrf_intervals/' + filename):
         print("File already exists: ", print(data_name, level, random_state, test_size))
         return
 
     results = {}
 
     results['data_name'] = data_name
-    results['n_neighbors'] = np.nan
+    results['n_neighbors'] = 'qrf'
     results['level'] = level
     results['random_state'] = random_state
     results['test_size'] = test_size
@@ -75,8 +75,7 @@ def interval_loop(data_name = None, level = 0.95, random_state = 42, test_size =
     # rf = RFGAP(oob_score = True, random_state = random_state, y = y_test)
     # rf.fit(x_train, y_train)
 
-    # results['oob_score'] = rf.oob_score_
-    # results['test_score'] = rf.score(x_test, y_test)
+
 
     # y_pred_test, y_pred_lwr_test, y_pred_upr_test = rf.predict_interval(X_test = x_test,
     #                                                                 n_neighbors = n_neighbors,
@@ -84,28 +83,22 @@ def interval_loop(data_name = None, level = 0.95, random_state = 42, test_size =
 
 
     upr_quantile = 1 - (1 - level) / 2
-    lwr_quantiles = (1 - level) / 2
+    lwr_quantile = (1 - level) / 2
 
-    rfqr = RandomForestQuantileRegressor(random_state = random_state)
-    rfqr.fit(X_train, y_train)
-    upper = np.concatenate((upper, rfqr.predict(X_test, quantile=98.5))) #TODO: set quantiles
-    lower = np.concatenate((lower, rfqr.predict(X_test, quantile=2.5)))
+    qrf = RandomForestQuantileRegressor(random_state = random_state)
+    qrf.fit(x_train, y_train)
 
-    # interval = upper - lower
-    # sort_ind = np.argsort(interval)
-    # y_true_all = y_true_all[sort_ind]
-    # upper = upper[sort_ind]
-    # lower = lower[sort_ind]
-    # mean = (upper + lower) / 2
+    lwr = qrf.predict(x_test, quantiles = lwr_quantile)
+    # med = qrf.predict(x_test, quantiles = 0.5)
+    upr = qrf.predict(x_test, quantiles = upr_quantile)
 
-    # Center such that the mean of the prediction interval is at 0.0
-    y_true_all -= mean
-    upper -= mean
-    lower -= mean
+    # results['oob_score'] = qrf.predict(x_train, quantiles = 0.5, oob_score = True)
 
-    results['coverage'] = get_coverage(y_test, y_pred_lwr_test, y_pred_upr_test)
+    results['oob_score'] = np.nan
+    results['test_score'] = np.nan
+    results['coverage'] = get_coverage(y_test, lwr, upr)
 
-    width_stats = get_width_stats(y_pred_lwr_test, y_pred_upr_test)
+    width_stats = get_width_stats(lwr, upr)
     results['width_mean'] = width_stats[0]
     results['width_sd'] = width_stats[1]
     results['width_min'] = width_stats[2]
@@ -114,7 +107,7 @@ def interval_loop(data_name = None, level = 0.95, random_state = 42, test_size =
     results['width_q3'] = width_stats[5]
     results['width_max'] = width_stats[6]
 
-    with open('intervals/' + filename, 'w') as f:
+    with open('qrf_intervals/' + filename, 'w') as f:
         json.dump(results, f)
 
 
@@ -128,12 +121,13 @@ if __name__ == '__main__':
 
     data_names = ['AirfoilSelfNoise', 'AirQuality', 'Automobile', 'AutoMPG',
                 'BeijingPM25', 'CommunityCrime', 'ComputerHardware',
-                'ConcreateCompressiveStrength', 'ConcreteSlumpTest', 
+                'ConcreteCompressiveStrength', 'ConcreteSlumpTest', 
                 'CyclePowerPlant', 'EnergyEfficiency', 'FacebookMetrics',
                 'FiveCitiesPM25', 'Hydrodynamics', 'IstanbulStock', 
                 'Naval Propulsion Plants', 'OpticalNetwork', 
                 'Parkinsons', 'Protein', 'SML2010']
     
+   
 
     levels = [0.80, 0.90, 0.95, 0.99]
     test_sizes = [0.2]
