38c06d2807749756cd028d24a85e6dbc7d60920b
aumona <adrien.aumon@umontreal.ca>
2026-03-28 01:33:41 -0400

    update setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding = "utf-8") as f:
     long_description = f.read()
 
 install_requires = ['scikit-learn', 'numpy', 'pandas', 'scipy', 'packaging']
 
-setuptools.setup(
+setup(
     name = 'rfgap',
     version = '0.0.1',
     author = 'Jake Rhodes',
@@ -14,6 +14,6 @@ setuptools.setup(
     long_description = long_description,
     url = 'https://github.com/jakerhodes/RF-GAP-Python',
     license = 'GNU-V3',
-    packages = ['rfgap'],
+    packages=find_packages(include=["rfgap", "rfgap.*"]),
     install_requires = install_requires
 )
